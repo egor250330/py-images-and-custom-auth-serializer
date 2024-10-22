6e40bfcff0f10478bb5d566a1da176c42129aba6
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-10-22 13:05:37 +0300

    refactor helper.py

--- cinema/helper.py
+++ cinema/helper.py
@@ -1,16 +1,15 @@
 import os
 import pathlib
 import uuid
-
 from django.utils.text import slugify
-from cinema.models import Movie
 
 
 def movies_image_path(movie: "Movie", filename: str) -> str:
+    from cinema.models import Movie
+
     filename = (
         f"{slugify(movie.title)}-"
         f"{uuid.uuid4()}"
         f"{pathlib.Path(filename).suffix}"
     )
-
     return os.path.join("upload-image", filename)
